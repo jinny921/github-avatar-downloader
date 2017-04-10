82de82a604a1bf2caf9a52dbc93e8a003a5d7e77
Jinny Huang <jinny921@gmail.com>
2017-04-10 23:19:02 +0000

    started the getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getRepoContributors('jquery', 'jquery', function(err, result) {
+  console.log(`Error: ${err}`);
+  console.log(`Result: ${result}`);
+})
