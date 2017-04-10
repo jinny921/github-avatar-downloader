90763a16e5dbdc2a8550e8cd6de35ee66b48a9a3
Jinny Huang <jinny921@gmail.com>
2017-04-10 23:42:02 +0000

    created the requestURLgit status

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,19 @@
+const GITHUB_USER = "jinny921";
+const GITHUB_TOKEN = "bafb585d2e313075a75f771f8d0dff7c4010813c";
+
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+  // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL);
 }
 
-getRepoContributors('jquery', 'jquery', function(err, result) {
+getRepoContributors('jquery', 'jquery', (err, result) => {
   console.log(`Error: ${err}`);
   console.log(`Result: ${result}`);
 })
+
