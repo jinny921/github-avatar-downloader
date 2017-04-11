74865f9e6418f637747614fe24513881b1735e68
Jinny Huang <jinny921@gmail.com>
2017-04-11 00:40:52 +0000

    making the request and console logged the body!

--- download_avatars.js
+++ download_avatars.js
@@ -2,18 +2,32 @@ const GITHUB_USER = "jinny921";
 const GITHUB_TOKEN = "bafb585d2e313075a75f771f8d0dff7c4010813c";
 
 const request = require('request');
+const fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  // var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  var requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
+  var requestOptions = {
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'Jinny Macbook OSX v11.2'
+    }
+  };
+  // console.log(requestURL);
+
+  request.get(requestOptions, cb);
 }
 
-getRepoContributors('jquery', 'jquery', (err, result) => {
-  console.log(`Error: ${err}`);
-  console.log(`Result: ${result}`);
-})
+function jinnyCallback(err, result, body) {
+  if (result) {
+    console.log(body);// do stuff with result
+  } else {
+    console.log("it's an error!!!" + err.code);
+  }
 
+}
+
+getRepoContributors('jquery', 'jquery', jinnyCallback)
